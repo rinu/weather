c2d029a3d545c5f3cdce8b4942b33f4e32f3130e
Rauno Moisto <rmoisto@gmail.com>
2021-03-05 15:12:56 +0200

    move token to env conf

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -3,14 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Location } from './location';
 import { Weather } from './weather';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
   getWeatherNow (location: Location): Observable<Weather> {
-    const appId = 'bd5117c50a8b3ab7984d451bb8b799b8';
-    return this.httpClient.get(`http://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&units=metric&appid=${appId}`) as Observable<Weather>;
+    return this.httpClient.get(`http://api.openweathermap.org/data/2.5/weather?lat=${location.lat}&lon=${location.lon}&units=metric&appid=${environment.weatherToken}`) as Observable<Weather>;
   }
 
   constructor(private httpClient: HttpClient) { }
